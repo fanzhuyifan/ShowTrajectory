fc3367b8606ea5a139191966378ccff3b262fffe
fanzhuyifan <fanzhuyifan@126.com>
2020-06-12 13:28:05 -0700

    Initial commit

---
+++ show_trajectory/Mod.cs
@@ -0,0 +1,14 @@
+using System;
+using Modding;
+using UnityEngine;
+
+namespace ShowTrajectory
+{
+	public class Mod : ModEntryPoint
+	{
+		public override void OnLoad()
+		{
+			// Called when the mod is loaded.
+		}
+	}
+}
